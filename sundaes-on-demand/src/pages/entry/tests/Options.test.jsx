3fc63d78f3856c9d341878d96d9c6c6d970a26ab
ManzeZhou <manzezz1996@gmail.com>
2023-01-13 23:37:38 -0500

    code toppingOption test alt

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,17 @@ test("displays image for each scoop option from server", async () => {
     const altText = scoopImages.map((element) => element.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each toppings option from server", async () => {
+    render(<Options optionType='toppings' />);
+
+    // find images
+    const images = await screen.findAllByRole("img", { name: /topping$/i });
+    expect(images).toHaveLength(3);
+
+    // confirm alt text of images
+    // @ts-ignore
+    const imageTitles = images.map((img) => img.alt);
+    expect(imageTitles).toEqual(["Cherries topping", "M&Ms topping", 'Hot fudge topping']);
+});
+
