7deff8338bb86614e67829de283493fabd320453
ManzeZhou <manzezz1996@gmail.com>
2023-01-16 17:50:11 -0500

    code error test and set error to true

---
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,34 @@
+import {server} from "../../../mocksServer/server";
+import {rest} from "msw";
+import {render, screen, waitFor} from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+
+//run only one test in a file to isolate a test
+test.only('handles errors for scoops and toppings routes', async () => {
+// overwrite handlers
+    server.resetHandlers(
+        rest.get('http://localhost:3030/scoops', (req,res,ctx) =>
+            res(ctx.status(500))
+        ),
+        rest.get('http://localhost:3030/toppings', (req,res,ctx) =>
+            res(ctx.status(500))
+        )
+    );
+
+    render(<OrderEntry />);
+
+    // situation: await for only one alterBanner and then run the following code without finding the second
+    // solution: wait for
+    waitFor (async () => {
+        const alerts = await screen.findAllByRole('alert');
+
+        expect(alerts).toHaveLength(2);
+    })
+});
+
+
+// run other test but skip one test
+test.skip('not a real test', () => {
+
+})
+
